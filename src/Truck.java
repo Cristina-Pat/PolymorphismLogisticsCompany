b26e7649e95bca10466a75b4266acf9419deaa93
Cristina Pat <calota.cristina@gmail.com>
2022-05-03 14:56:43 +0100

    added Truck class

--- src/Truck.java
+++ src/Truck.java
@@ -1,2 +1,10 @@
 
+public class Truck extends Shipment{
 
+	private String driver;
+	
+	public Truck(int aLoad, String aDestination, int anETA, String aDriver) {
+		super(aLoad, aDestination, anETA);
+		driver = aDriver;
+	}
+}
